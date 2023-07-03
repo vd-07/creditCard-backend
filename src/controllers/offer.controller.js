c0ea17af2b1fafd5f07b020ed1cd90bbb5c3409b
--local <dubey.vivek.2805@gmail.com>
2023-07-04 02:49:55 +0530

    implementing offer updation

--- src/controllers/offer.controller.js
+++ src/controllers/offer.controller.js
@@ -69,5 +69,64 @@ exports.getOffer = async (req, res) => {
 
 // update offer touches two tables, so the operation should be atomic, can use ticket based design
 exports.updateOffer = async (req, res) => {
+    const { limitOfferId, status } = req.body;
 
+    if(status != "ACCEPTED" && status != "REJECTED") {
+        return res.status(400).json({ message: 'Wrong status value' });
+    }
+    // Check if the offer exists and is in a valid status
+    const checkOfferQuery = `
+        SELECT * 
+        FROM offer 
+        WHERE limit_offer_id = $1
+        AND status = 'PENDING';
+    `;
+    try {
+        const { rows } = await db.query(checkOfferQuery, [limitOfferId]);
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Offer not found or cannot be updated.' });
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: 'Error retrieving the offer from the database.' });
+    }
+
+     // Update the offer status
+    const updateOfferQuery = `
+        UPDATE offer 
+        SET status = $1
+        WHERE limit_offer_id = $2
+        RETURNING account_id, limit_type, new_limit;
+    `;
+    try {
+    const { rows } = await db.query(updateOfferQuery, [status, limitOfferId]);
+    const { account_id, limit_type, new_limit } = rows[0];
+
+    if (status == 'ACCEPTED') {
+        // Update the account limit values based on the limit type
+        if (limit_type == 'ACCOUNT_LIMIT') {
+            const updateAccountQuery = `
+                UPDATE customer 
+                SET last_account_limit = account_limit,
+                    account_limit = $1,
+                    account_limit_update_time = current_date
+                WHERE account_id = $2;
+            `;
+            await db.query(updateAccountQuery, [new_limit, account_id]);
+        } else if (limit_type == 'PER_TRANSACTION_LIMIT') {
+            const updateAccountQuery = `
+                UPDATE customer 
+                SET last_per_transaction_limit = per_transaction_limit,
+                    per_transaction_limit = $1,
+                    per_transaction_limit_update_time = current_date
+                WHERE account_id = $2;
+            `;
+            await db.query(updateAccountQuery, [new_limit, account_id]);
+        }
+    }
+        return res.status(200).json({ message: 'Offer status updated successfully.' });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: 'Error updating the offer status.' });
+    }
 };
